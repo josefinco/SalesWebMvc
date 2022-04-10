3318241f437e53f4a79e2fae1250cf9559581860
José Finco <josefinco_@hotmail.com>
2022-04-10 09:56:11 -0300

    Adicionar funcionalidade de Eager loading para department do seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,6 +1,8 @@
-﻿using SalesWebMvc.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
+
 
 namespace SalesWebMvc.Services
 {
@@ -34,7 +36,7 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
     }
 }
